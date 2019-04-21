d11ea8d5762358c3a06903103ddcdcd19d7197c0
Eric Werbel <enw12@duke.edu>
2019-04-21 14:06:43 -0400

    Communication working

--- Processor Code/skeleton.v
+++ Processor Code/skeleton.v
@@ -27,67 +27,17 @@ module skeleton(clock, reset, // data_writeReg, ctrl_writeReg, ctrl_writeEnable,
 	 reg [7:0] out_reg;
 	 assign out_signal = out_reg;
 	 
+	 reg [7:0] Rx_data;
+	 
 	 reg transmit;
 	 wire t_enable, transmit_active;
-//	 wire out_inv;
-	
-//	 async_transmitter myTransmit(.clk(clock), .TxD_start(~busy), .TxD_data(out_reg), .TxD(out), .TxD_busy(busy));	
-//	 UART_rs232_tx myTransmit(.Clk(clock), .Rst_n(~reset), .TxEn(TxEnable), .TxData(out_reg), .TxDone(TxDone), 
-//			.Tx(out), .Tick(tick), .NBits(4'b1000));
-//			
-//	 UART_BaudRate_generator(.Clk(clock), .Rst_n(~reset), .Tick(tick), .BaudRate(16'd325));
-			
-
-//wire				Tx;
-//wire [7:0]    	TxData     	; // Data to transmit.
-//wire          	RxDone          ; // Reception completed. Data is valid.
-//wire          	TxDone          ; // Trnasmission completed. Data sent.
-//wire            tick		; // Baud rate clock
-//reg          	TxEn            ;
-//wire 		RxEn		;
-//wire [3:0]      NBits    	;
-//wire [15:0]    	BaudRate        ; //328; 162 etc... (Read comment in baud rate generator file)
-///////////////////////////////////////////////////////////////////////////////////////////
-//assign 		RxEn = 1'b1	;
-////assign 		BaudRate = 16'd325; 	//baud rate set to 9600 for the HC-06 bluetooth module. Why 325? (Read comment in baud rate generator file)
-//assign		BaudRate = 16'd5;
-//assign 		NBits = 4'b1000	;	//We send/receive 8 bits
-/////////////////////////////////////////////////////////////////////////////////////////
-
-
-//Make connections between Rx module and TOP inputs and outputs and the other modules
-//UART_rs232_rx I_RS232RX(
-//    	.Clk(clock)             	,
-//   	.Rst_n(~reset)         	,
-//    	.RxEn(RxEn)           	,
-//    	.RxData(RxData)       	,
-//    	.RxDone(RxDone)       	,
-//    	.Rx(Rx)               	,
-//    	.Tick(tick)           	,
-//    	.NBits(NBits)
-//    );
-
-//Make connections between Tx module and TOP inputs and outputs and the other modules
-//UART_rs232_tx I_RS232TX(
-//   	.Clk(clock)            	,
-//    	.Rst_n(~reset)         	,
-//    	.TxEn(1'b1)           	,
-//    	.TxData(out_reg)      	,
-//   	.TxDone(TxDone)      	,
-//   	.Tx(Tx)               	,
-//   	.Tick(tick)           	,
-//   	.NBits(NBits)
-//    );
 
 
 		assign t_enable = ~transmit_active & transmit;
 
-//		uart_tx myTransmit(.i_Clock(clock), .i_Tx_DV(t_enable), .i_Tx_Byte(out_reg), 
-//			.o_Tx_Active(transmit_active), .o_Tx_Serial(out), .o_Tx_Done());	
-
 		test myTransmit(
-				.from_uart_ready(transmit), 						// avalon_data_receive_source.ready
-				.from_uart_data(),  						//                           .data
+				.from_uart_ready(), 						// avalon_data_receive_source.ready
+				.from_uart_data(Rx_data),  						//                           .data
 				.from_uart_error(), 						//                           .error
 				.from_uart_valid(), 						//                           .valid
 				.to_uart_data(out_reg),    			//  avalon_data_transmit_sink.data
@@ -95,22 +45,13 @@ module skeleton(clock, reset, // data_writeReg, ctrl_writeReg, ctrl_writeEnable,
 				.to_uart_valid(transmit), 		  				//                           .valid
 				.to_uart_ready(),   						//                           .ready
 				.clk(clock),          				   //                        clk.clk
-				.UART_RXD(),        						//         external_interface.RXD
+				.UART_RXD(Rx),        						//         external_interface.RXD
 				.UART_TXD(out),      				   //                           .TXD
 				.reset(reset)   
 		);
 
-//		assign out = ~out_inv;
-				
-//Make connections between tick generator module and TOP inputs and outputs and the other modules
-//UART_BaudRate_generator I_BAUDGEN(
-//    	.Clk(clock)               ,
-//    	.Rst_n(~reset)           ,
-//    	.Tick(tick)             ,
-//    	.BaudRate(BaudRate)
-//    );
-			
-			
+		
+	
 	 always @(posedge clock)
 	 begin
 	 	
@@ -184,11 +125,11 @@ module skeleton(clock, reset, // data_writeReg, ctrl_writeReg, ctrl_writeEnable,
     );
 
     /** PROCESSOR **/
-	 assign custom_in = 32'd3;
+	 assign forward_data = Rx_data;
 	 
     processor my_processor(
         // Custom
-		  custom_in,
+		  forward_data,
 		  stop,
 		  
 		  // Control signals
